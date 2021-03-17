306aedf4ee75f42344e2ec96cee05bf4ef5a3f1c
monicatevy <monicatevy.sen@protonmail.com>
2021-03-18 00:39:50 +0700

    edit card in html/css (attempt folder)

--- js/StudioGhibli.js
+++ js/StudioGhibli.js
@@ -28,18 +28,22 @@ request.onload = function () {
       const h1 = document.createElement('h1');
       h1.textContent = movie.title;
 
+      const h2 = document.createElement('h2');
+      h2.textContent = movie.original_title;
+
       const p = document.createElement('p');
       movie.description = movie.description.substring(0, 300);
       p.textContent = `${movie.description}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
+      card.appendChild(h2);
       card.appendChild(p);
       });
 
   } else {
-    const h2 = document.createElement('h2');
-    h2.textContent = "error";
+    const h3 = document.createElement('h3');
+    h3.textContent = "error";
 
     app.appendChild(h2);
   }
